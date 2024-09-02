0e56b068a03786bff42aa043fb9235e406bdb8ae
Mulyana N <mulyanan.ur26@gmail.com>
2024-09-02 16:54:23 +0700

    Create Controllet Get EndPoint

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2280793f0f2bd0e305c35affe32c8581f241f50")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5efc6ef2fc22cdb35823b6c180774a068ca2f38")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
